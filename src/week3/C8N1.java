4813ec61e4e2137bce80fee9a9e457456efe4021
Matt Getz <getzmc26@mounties.mansfield.edu>
2017-02-06 19:19:55 -0500

    Initial commit.

--- src/week3/C8N1.java
+++ src/week3/C8N1.java
@@ -9,42 +9,66 @@ import java.util.Scanner;
 public class C8N1 {
 
     public static String colSum() {
+
+//Read in total number of rows and the number of numbers in each row.        
         Scanner sc = new Scanner(System.in);
         System.out.print("How many rows?: ");
         int n = sc.nextInt();
         int[][] x = new int[n][];
-        
+        int[] y = new int[n];
+
+        System.out.println("\n");
+
+        int largestRow = 0;
         int rowNum = 1;
         for (int i = 0; i < n; i++) {
-            System.out.println("\n");
-            x[i] = new int[n];
-            for (int j = 0; j < n; j++) {
-                System.out.print("Enter the number at index [" + i + "][" + j + "]: ");
-                x[i][j] = sc.nextInt();
+            System.out.print("How many numbers are in row " + rowNum + "?: ");
+            y[i] = sc.nextInt();
+            if (y[i] > largestRow) {
+                largestRow = y[i];
             }
             rowNum++;
         }
         System.out.println("\n");
+
+//Store user entered numbers for each row. Each row is of largest row size.      
+        int rowNumber = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.println("\n");
+            System.out.println("Row " + rowNumber + ": ");
+            System.out.println("------");
+            x[i] = new int[largestRow];
+            for (int j = 0; j < y[i]; j++) {
+                System.out.print("Enter the number at index [" + i + "][" + j + "]: ");
+                x[i][j] = sc.nextInt();
+            }
+            rowNumber++;
+        }
+        System.out.println("\n");
+
+//Print multi-dimensional array.        
         for (int row = 0; row < x.length; row++) {
             for (int column = 0; column < x[row].length; column++) {
                 System.out.print(x[row][column] + " ");
             }
             System.out.println("");
         }
-        int colNum = 1;
         System.out.println("\n");
-        for (int col = 0; col < x.length; col++) {
+
+//Sum columns and print each sum.        
+        int colNum = 1;
+        for (int col = 0; col < largestRow; col++) {
             int sum = 0;
-            for (int row = 0; row < x.length; row++) {
+            for (int row = 0; row < largestRow; row++) {
                 sum += x[row][col];
             }
             System.out.print("The sum of column " + colNum + " is: " + sum);
             colNum++;
             System.out.println("\n");
-        }    
+        }
+        System.out.println("");
     return "";    
     }
-    
     public static void main(String[] args) {
         System.out.println(colSum());
     }

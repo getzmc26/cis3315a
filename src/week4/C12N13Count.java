81774b4f9b2c6742c95eb6fb7ba8b78956ed35c6
Matt Getz <getzmc26@mounties.mansfield.edu>
2017-02-22 23:40:31 -0500

    Re-factored class names to list correctly.

---
+++ src/week4/C12N13Count.java
@@ -0,0 +1,14 @@
+package week4;
+
+/**
+ * Write a program that will count the number of chars, words, and lines in a
+ * file.
+ *
+ * @author Matt Getz
+ */
+public class C12N13Count {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/week4/C13N16Calculator.java
@@ -0,0 +1,13 @@
+package week4;
+
+/**
+ * Create a rational-number calculator. Practice problem 7.9 is similar.
+ *
+ * @author Matt
+ */
+public class C13N16Calculator {
+
+    public static void main(String[] args) {
+
+    }
+}

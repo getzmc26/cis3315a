5ad81e4a78d50e71cbff8459a2e0abc0d32ff1b4
Matt Getz <getzmc26@mounties.mansfield.edu>
2017-02-23 08:49:32 -0500

    Added clone method and toString.

--- src/week4/C13N17ComplexNumber.java
+++ src/week4/C13N17ComplexNumber.java
@@ -7,9 +7,17 @@ package week4;
  */
 public class C13N17ComplexNumber {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Complex c1 = new Complex(3.5, 5.5);
-        System.out.println(c1);
+        Complex c2 = (Complex)c1.clone();
+        System.out.println("\nc1 = " + c1);
+        System.out.println("\nc2 = " + c2);
+        c2.setA(5.5);
+        c2.setB(8.5);
+        System.out.println("\nc1 = " + c1);
+        System.out.println("\nc2 = " + c2);
+        System.out.println("\nc2 - c1 = " + c2.subtract(c1));
+        
     }
 }
 
@@ -46,6 +54,10 @@ class Complex implements Cloneable {
 
     public Complex subtract(Complex c) {
         return new Complex((a - c.getA()), (b - c.getB()));
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 
     @Override

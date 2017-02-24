a272a9cbd52793fa31db80e60715b7dd70eae1d4
Matt Getz <getzmc26@mounties.mansfield.edu>
2017-02-23 22:08:00 -0500

    Initial commit.

---
+++ week3/Chess.java
@@ -0,0 +1,35 @@
+package week3;
+
+/**
+ * Chess board / future game.
+ * @author Matt Getz
+ */
+public class Chess {
+    
+    public static void display(char[][] b){
+        for(int row=0; row<b.length; row++) {
+            for(int col=0; col<b[row].length; col++) {
+                System.out.print(b[row][col] + " ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public static void main(String[] args) {
+        char[][] board = {
+            {'r','h','b','q','k','b','h','r'},
+            {'p','p','p','p','p','p','p','p'},
+            {'-','-','-','-','-','-','-','-'},
+            {'-','-','-','-','-','-','-','-'},
+            {'-','-','-','-','-','-','-','-'},
+            {'-','-','-','-','-','-','-','-'},
+            {'P','P','P','P','P','P','P','P'},
+            {'R','H','B','Q','K','B','H','R'},
+        };  
+    display(board);
+    board[0][1] = '-';
+    board[2][2] = 'h';
+    System.out.println("\n");
+    display(board);
+    }
+}
---
+++ week3/ChessUI.java
@@ -0,0 +1,61 @@
+package week3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Matt Getz
+ */
+public class ChessUI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        ChessBoard chess = new ChessBoard();
+        int x = chess.count;
+        int[] moveFrom = new int[2];
+        int[] moveTo = new int[2];
+        
+        boolean endGame = false;
+        
+        System.out.println(chess);
+        firstLoop:
+        while (endGame == false) {
+            if (x % 2 == 0) {
+                System.out.println("Turn player WHITE:");
+            }
+            else {
+                System.out.println("Turn player BLACK:");
+            }
+            while (true){
+                System.out.print("Play a move = 1 , Quit = 2: ");
+                String ans = sc.next();
+                if (ans.equals("1")) {
+                    break;
+                }
+                else if (ans.equals("2")) {
+                    break firstLoop;
+                }
+                else {
+                    System.out.println("Please enter a 1 or a 2.");
+                }
+            }
+            System.out.print("Enter move from (ex. 1 0) : ");
+                for (int i=0; i<2; i++) {
+                    moveFrom[i] = sc.nextInt();
+                }
+            System.out.print("Enter move to (ex. 1 0) : ");
+                for (int i=0; i<2; i++) {
+                    moveTo[i] = sc.nextInt();
+                }
+            System.out.println("\n");    
+            System.out.println("Move from " + moveFrom[0] + "," + moveFrom[1] +
+                " to " + moveTo[0] + "," + moveTo[1]);
+            chess.move(moveFrom[0], moveFrom[1], moveTo[0], moveTo[1]);
+            System.out.println("\n");
+            System.out.println(chess);
+            x++;
+        }
+        System.out.println("\n");
+        System.out.println("Thanks for playing!");
+    }
+}

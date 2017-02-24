076a9596e2498681611a159687058c881544ccba
Matt Getz <getzmc26@mounties.mansfield.edu>
2017-02-23 20:15:33 -0500

    Added file output when program runs. Formatted both for readability (though a toString method should be used when printing).

--- src/week4/C12N24SalaryFile.java
+++ src/week4/C12N24SalaryFile.java
@@ -1,7 +1,10 @@
 package week4;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
@@ -10,28 +13,9 @@ import java.io.PrintWriter;
  */
 public class C12N24SalaryFile {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("Salary.txt");
-
-        if (file.exists()) {
-            System.out.println("File " + file.getName() + " already exists.");
-            System.exit(0);
-        }
-        String rank = "";
-        double salary;
-        try (
-                PrintWriter output = new PrintWriter(file);) {
-            for (int i = 1; i <= 1000; i++) {
-                output.print("FirstName " + i + " LastName " + i);
-                rank = getRank();
-                salary = getSalary(rank);
-                output.printf(" " + rank + " %.2f\n", salary);
-            }
-        }
-    }
-
     public static String getRank() {
-        String[] ranks = {"Assistant", "Associate", "Full"};
+        String[] ranks = {"        Assistant        ",
+            "        Associate        ", "        Full             "};
         return ranks[(int) (Math.random() * ranks.length)];
     }
 
@@ -42,6 +26,32 @@ public class C12N24SalaryFile {
             return 60000 + (double) (Math.random() * 50001); //$110,000 cap
         } else {
             return 75000 + (double) (Math.random() * 55001); //$130,000 cap
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        File file = new File("Salary.txt");
+
+        if (!file.exists()) {
+            System.out.println("File " + file.getName() + " already exists.");
+            System.exit(0);
+        }
+        String rank = "";
+        double salary;
+        try (PrintWriter output = new PrintWriter(file);) {
+            output.printf("FirstName       LastName             Rank             Salary\n");
+            output.printf("-----------------------------------------------------------------\n");
+            for (int i = 1; i <= 1000; i++) {
+                output.print("FirstName   " + i + "   LastName   " + i);
+                rank = getRank();
+                salary = getSalary(rank);
+                output.printf(" " + rank + "$" + " %.2f\n", salary);
+                output.printf("\n");
+            }
+        }
+        BufferedReader br = new BufferedReader(new FileReader("Salary.txt"));
+        for (String line; (line = br.readLine()) != null;) {
+            System.out.print(line + "\n");
         }
     }
 }
